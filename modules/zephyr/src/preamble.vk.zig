2c95fe06843de1bb76bd8391e0db4da398105b90
Dan Liebault <dan.liebault@viacesi.fr>
2026-08-27 00:40:50 +0200

    validation was not part of vk 1

--- modules/zephyr/src/preamble.vk.zig
+++ modules/zephyr/src/preamble.vk.zig
@@ -42,7 +42,6 @@ pub fn check_result(result: Result) Error!void {
         .error_full_screen_exclusive_mode_lost_ext => return Error.FullScreenExclusiveModeLostExt,
         .error_out_of_date_khr => return Error.OutOfDateKHR,
         .error_surface_lost_khr => return Error.SurfaceLostKHR,
-        .error_validation_failed => return Error.ValidationFailed,
         else => return Error.Unknown,
     }
 }
